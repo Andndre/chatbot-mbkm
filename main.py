9d41e39e94f78ca8ff638edd9adbb5b8a48c4460
andndre <agungandre687@gmail.com>
2024-03-07 20:51:07 +0800

    /hello bot

---
+++ main.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+if __name__ == "__main__":
+	app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
+	app.add_handler(CommandHandler("hello", hello))
+	app.run_polling()
